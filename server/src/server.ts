cd248bf02a50d1fc3be5ce9aec882176707c4906
Herik Rocha <herik.rocha@thomsonreuters.com>
2020-06-07 14:43:17 -0300

    First commit

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express, { response } from 'express'; //Código
+import cors from 'cors';
+import path from 'path'; 
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))); //servir arquivos estaticos // '..' -> voltar um diretorio
+
+app.listen(3333);
